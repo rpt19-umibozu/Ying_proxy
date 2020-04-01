56225cc178ecc81a79da2b00b801f7e73b8b8d93
Yingwen <inwen.chen@gmail.com>
2020-03-31 19:48:18 -0600

    four modules rendering on same page

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
@@ -11,6 +12,26 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(express.static(('public')));
+
+
+// var serverOne = 'http://localhost:3001',
+//     ServerTwo = 'http://localhost:3002',
+//     ServerThree = 'http://localhost:3004';
+
+// app.all('/', function(req, res) {
+//     console.log('redirecting to Server1');
+//     apiProxy.web(req, res, {target: serverOne});
+// });
+
+// app.all('/', function(req, res) {
+//     console.log('redirecting to Server2');
+//     apiProxy.web(req, res, {target: ServerTwo});
+// });
+
+// app.all('/', function(req, res) {
+//     console.log('redirecting to Server3');
+//     apiProxy.web(req, res, {target: ServerThree});
+// });
 //console.log(express.static(__dirname + 'public'))
 var port = 3000;
 
